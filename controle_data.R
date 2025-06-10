d84f0ccea630910917b61d8abbf4c6e6be42a5b6
Lex0803 <Lexveerman@gmail.com>
2025-06-10 13:36:48 +0200

    Add files via upload

---
+++ controle_data.R
@@ -0,0 +1,3 @@
+
+onderwijsniveau_regio <- read.csv("C:/Users/Veerman/Downloads/Hoogst_behaald_onderwijsniveau__regio_05062025_113647.csv", sep=";")
+werkeloosheid_provincie <- read.csv2("C:/Users/Veerman/Downloads/Werklozename (% van beroepsbevolking).csv")
